6bd3a3bf98c29c7f4ad8e8aeb1dd1fb972d021e0
Gal Farfel <Gal.Farfel@e.braude.ac.il>
2023-12-24 23:54:39 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+import os
+from dotenv import load_dotenv
+import datetime
+
+# Load environment variables into programming variables.
+load_dotenv()
+openai_api_key = os.getenv('OPEN_AI_KEY')
+bot_api_key = os.getenv('BOT_API_KEY')
+phone = os.getenv('PHONE')
+
+# Get today's day and month and convert the month into a string-month.
+current_month = datetime.datetime.now().month
+current_day = datetime.datetime.now().day
+months_to_names = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
+# Inject today's day and month into the url.
+link_url = f"""
+    *{current_day}/{current_month} Musical History:*
+    https://www.thisdayinmusic.com/on-this-day-in-music-{months_to_names[current_month-1]}-{current_day}/
+"""
+# Construct the callmebot API url with today's parameters.
+url = f"https://api.callmebot.com/whatsapp.php?phone={phone}&text={link_url}&apikey={bot_api_key}"
+
+# Send the whatsapp message by sending a GET request for the API.
+response = requests.get(url)
+
