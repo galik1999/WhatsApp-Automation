387f6bf94d916358c556d4d42165925f60e49dbd
Gal Farfel <Gal.Farfel@e.braude.ac.il>
2023-12-30 16:57:38 +0200

    added if name == main

--- main.py
+++ main.py
@@ -3,38 +3,44 @@ import os
 from dotenv import load_dotenv
 import datetime
 
-# # Load environment variables into programming variables.
-# load_dotenv()
-# bot_api_key = os.environ['BOT_API_KEY']
-# phone = os.environ['PHONE']
-#
-# # Get today's day and month and convert the month into a string-month.
-# current_month = datetime.datetime.now().month
-# current_day = datetime.datetime.now().day
-# months_to_names = [
-#     'January',
-#     'February',
-#     'March',
-#     'April',
-#     'May',
-#     'June',
-#     'July',
-#     'August',
-#     'September',
-#     'October',
-#     'November',
-#     'December'
-# ]
-#
-# # Inject today's day and month into the url.
-# link_url = f"""
-#     *{current_day}/{current_month} Musical History:*
-#     https://www.thisdayinmusic.com/on-this-day-in-music-{months_to_names[current_month-1]}-{current_day}/
-# """
-# # Construct the callmebot API url with today's parameters.
-# url = f"https://api.callmebot.com/whatsapp.php?phone={phone}&text={link_url}&apikey={bot_api_key}"
-#
-# # Send the whatsapp message by sending a GET request for the API.
-# response = requests.get(url)
-#
-print('HELLO WORLD')
+
+def main():
+    # Load environment variables into programming variables.
+    load_dotenv()
+    bot_api_key = os.environ['BOT_API_KEY']
+    phone = os.environ['PHONE']
+
+    # Get today's day and month and convert the month into a string-month.
+    current_month = datetime.datetime.now().month
+    current_day = datetime.datetime.now().day
+    months_to_names = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+
+    # Inject today's day and month into the url.
+    link_url = f"""
+        *{current_day}/{current_month} Musical History:*
+        https://www.thisdayinmusic.com/on-this-day-in-music-{months_to_names[current_month - 1]}-{current_day}/
+    """
+    # Construct the callmebot API url with today's parameters.
+    url = f"https://api.callmebot.com/whatsapp.php?phone={phone}&text={link_url}&apikey={bot_api_key}"
+
+    # Send the whatsapp message by sending a GET request for the API.
+    # response = requests.get(url)
+
+    print('OK')
+
+
+if __name__ == '__main__':
+    main()
