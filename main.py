46224bdbe8532568868a08c8d68d5008e0a7a7b3
Gal Farfel <Gal.Farfel@e.braude.ac.il>
2023-12-30 17:08:38 +0200

    again

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import datetime
 def main():
     # Load environment variables into programming variables.
     load_dotenv()
-    bot_api_key = os.environ['BOT_API_KEY']
-    phone = os.environ['PHONE']
+    bot_api_key = os.getenv('BOT_API_KEY')
+    phone = os.getenv('PHONE')
 
     # Get today's day and month and convert the month into a string-month.
     current_month = datetime.datetime.now().month
@@ -39,7 +39,7 @@ def main():
     # Send the whatsapp message by sending a GET request for the API.
     response = requests.get(url)
 
-    print(response,url)
+    print(response, url)
 
 
 if __name__ == '__main__':
